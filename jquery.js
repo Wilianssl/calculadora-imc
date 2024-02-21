4d3ee91adeb0fb1b62c9b5608efd1a25cc55676f
Wilianssl <wiliancontato2@gmail.com>
2024-02-21 16:43:20 -0300

    first commit

---
+++ jquery.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('#altura').mask('0.00', { reverse: true });
+});
+
+$(document).ready(function() {
+    $("#calcular").click(function() {
+      var altura = parseFloat($("#altura").val());
+      var peso = parseFloat($("#peso").val());
+      
+      if (altura && peso) {
+        var imc = peso / (altura * altura);
+        var resultado = "Seu IMC é " + imc.toFixed(2) + ". ";
+        
+        if (imc < 18.5) {
+          resultado += "Você está abaixo do peso.";
+        } else if (imc >= 18.5 && imc < 25) {
+          resultado += "Você está com o peso normal.";
+        } else if (imc >= 25 && imc < 30) {
+          resultado += "Você está acima do peso.";
+        } else {
+          resultado += "Você está obeso.";
+        }
+        
+        $("#mostraResultado").text(resultado);
+      } else {
+        $("#mostraResultado").text("Por favor, preencha ambos os campos corretamente.");
+      }
+    });
+  });
